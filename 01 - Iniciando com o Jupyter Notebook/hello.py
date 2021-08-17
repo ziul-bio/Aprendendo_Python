4e647cf029db27f9886e62bfcaa6e536f8cc8c89
Luiz <ziul.bio@gmail.com>
2021-08-17 11:07:39 -0300

    Updating notebooks

---
+++ 01 - Iniciando com o Jupyter Notebook/hello.py
@@ -0,0 +1,3 @@
+print('Hello World')
+print('Útil para criar um arquivo .py pelo jupyter ou sobrescrever um arquivo .py existente')
+print('Neste caso com o uso de 2 %% todo o conteúdo da célula será inserido no arquivo hello.py')
